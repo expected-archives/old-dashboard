1fe7b5eb0d6777d3d6643be9df3508d32496795e
Rémi Caumette <remicaumette@icloud.com>
2019-03-02 20:10:40 +0100

    initial commit

---
+++ src/views/ListImage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Header } from "../components";
+
+export default () => (
+    <div>
+        <Header title={'Images'} pretitle={'Overview'} />
+    </div>
+);
